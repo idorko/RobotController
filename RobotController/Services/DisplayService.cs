ca40ce07b6e67a5724f5def87aa78d1a26b06dcb
Ian Dorko <ian.dorko@bdel.com>
2017-03-05 15:37:05 -0700

    added display service

---
+++ RobotController/Services/DisplayService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RobotController.Models;
+
+namespace RobotController.Services
+{
+    public class DisplayService
+    {
+        private List<Slot> slots;
+
+        public DisplayService(List<Slot> slots)
+        {
+            this.slots = slots;
+        }
+
+        public void DisplaySlots()
+        {
+            foreach (var slot in slots)
+            {
+                DisplaySlot(slot);
+            }
+        }
+
+        public void DisplaySlot(Slot slot)
+        {
+            StringBuilder output = new StringBuilder();
+            output.Append(string.Format("{0}:", slot.SlotId));
+            foreach (var block in slot.Blocks)
+            {
+                output.Append(string.Format(" {0}", block.Identifier));
+            }
+            Console.WriteLine(output);
+        }
+    }
+}
